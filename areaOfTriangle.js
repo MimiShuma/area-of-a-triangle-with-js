395f5e2b7781d6f578437b09dce7f0eef7eeb5fa
It's Mimi <shuma.ttt@gmail.com>
2024-05-22 14:10:38 +0600

    created an object of reusable methods for later use.

--- areaOfTriangle.js
+++ areaOfTriangle.js
@@ -1,18 +1,23 @@
 
-function getAreaOfTriangle(a, b, c) {
-    let semiPerimeter = (a + b + c) / 2;
-    let area = Math.sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
-    return area;
+let areaOfTriangleWithThreeSides = {
+    getAreaOfTriangle: function (a, b, c) {
+        let semiPerimeter = (a + b + c) / 2;
+        let area = Math.sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
+        return area;
+    },
+    getResult: function (sideA, sideB, sideC, targetDiv) {
+
+        let sideAA = parseFloat(document.getElementById(sideA).value);
+        let sideBB = parseFloat(document.getElementById(sideB).value);
+        let sideCC = parseFloat(document.getElementById(sideC).value);
+        let result = areaOfTriangleWithThreeSides.getAreaOfTriangle(sideAA, sideBB, sideCC);
+
+        let target = document.getElementById(targetDiv);
+        target.innerHTML = result;
+    }
 }
 
 
-function getResult() {
 
-    let sideA = parseFloat(document.getElementById('sideA').value);
-    let sideB = parseFloat(document.getElementById('sideB').value);
-    let sideC = parseFloat(document.getElementById('sideC').value);
-    let result = getAreaOfTriangle(sideA, sideB, sideC);
 
-    let target = document.getElementById('target');
-    target.innerHTML = result;
-}
+
